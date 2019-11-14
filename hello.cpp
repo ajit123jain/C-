5d31d0d14aceeb9e54f37253c2daa9eb7ad2158f
ajit123jain <ajitkumarjain123@gmail.com>
2019-11-15 00:52:05 +0530

    initial commit

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+/* hello.cpp */
+#include <iostream>
+using namespace std;
+int main()
+{
+	// This first line is comment in C++
+	cout << "Please enter your name: ";
+	string name;
+	cin >> name;
+	cout << "Hello, " << name;
+	cout << " You are handsome.";
+	cout << "\n";
+	return 0;
+}
